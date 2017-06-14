a5a7db64c52c0de96a83c6f8d985cdf4e955ad4f
Trialdev <coolbman200@gmail.com>
2017-06-14 19:33:57 -0400

    Added the 4 button prefab and managers

---
+++ rollRPG/Assets/Scripts/Managers/ButtonManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonManager : MonoBehaviour {
+    [SerializeField]
+    Button menuButton1;
+    [SerializeField]
+    Button menuButton2;
+    [SerializeField]
+    Button menuButton3;
+    [SerializeField]
+    Button menuButton4;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    /// <summary>
+    /// Returns an array of the 4 buttons in order of 1,2,3,4
+    /// </summary>
+    /// <returns></returns>
+    Button[] GetButtons()
+    {
+        Button[] temp = { menuButton1, menuButton2, menuButton3, menuButton4 };
+        return temp;
+    }
+}
