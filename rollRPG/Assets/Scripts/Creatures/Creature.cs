ea9fe9cba3ae93528b5c91c436e8e188aefc1be0
Trialdev <coolbman200@gmail.com>
2017-06-14 20:23:22 -0400

    Added Creature.cs and gave global Roll() function, and removed visual studio stuff from gits tracking

---
+++ rollRPG/Assets/Scripts/Creatures/Creature.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Creature  {
+
+    #region Resources
+    float currentHP;
+    float maxHP;
+
+    float currentEG;
+    float maxEG;
+
+    float currentMP;
+    float maxMP;
+    #endregion
+
+    #region Stats
+    int Strength;
+    int Dexterity;
+    int Consitution;
+    int Intelligence;
+    int Wisdom;
+    int Charisma;
+    int Armor;
+    #endregion
+
+    public enum STATS
+    {
+        STR,
+        DEX,
+        CON,
+        INT,
+        WIS,
+        CHA,
+        ARMOR,
+        NUMOFSTATS
+
+    }
+}
--- rollRPG/Assets/Scripts/Managers/GlobalVar.cs
+++ rollRPG/Assets/Scripts/Managers/GlobalVar.cs
@@ -2,15 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GlobalVar : MonoBehaviour {
+public static class GlobalVar {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    /// <summary>
+    /// This will roll a dice with (numOfSides) sides, and return the result
+    /// </summary>
+    /// <param name="numOfSides">The number of sides the dice has</param>
+    /// <returns></returns>
+	public static int Roll(int numOfSides)
+    {
+        return (int)((Random.value * 100) % numOfSides) + 1;
+    }
 }
